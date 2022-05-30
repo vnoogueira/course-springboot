4ca39da6ee51390f977f6112b9fd46a186046c7b
Vitor Nogueira <vitor.nogueira94@gmail.com>
2022-05-30 19:59:54 -0300

    Service Layer, component registration

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
