89173b2e2b52eb2196e406c2d94525918b2deb74
Vitor Nogueira <vitor.nogueira94@gmail.com>
2022-05-30 21:44:57 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.example.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.example.course.entities.Order;
 import com.example.course.entities.User;
+import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.UserRepository;
 
 @Configuration
@@ -17,13 +20,21 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private OrderRepository orderRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
 		User u1 = new User(null, "Jacqueline Xavier", "jacqueline@gmail.com", "789456123", "123456789");
 		User u2 = new User(null, "Vitor Nogueira", "vitor@gmail.com", "123456789", "789456123");
 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2015-06-20T18:23:05Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2018-06-20T17:47:01Z"), u1);
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 
 }
